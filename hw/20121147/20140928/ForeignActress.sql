0484f781ce46d402e958684d172672bde41dbae7
유은진 <20121147@prism.duksung.ac.kr>
2014-09-28 13:35:03 +0900

    FirstTable

--- hw/20121147/20140928/ForeignActress.sql
+++ hw/20121147/20140928/ForeignActress.sql
@@ -9,7 +9,7 @@ insert into ForeignActress (AName,ABirth,AHeight,ASchool,AFirstMovie,ALover) val
 insert into ForeignActress (AName,ABirth,AHeight,ASchool,AFirstMovie,ALover) values ('스칼렛 요한슨',1984,160,'초 졸','소피스트리','n');
 insert into ForeignActress (AName,ABirth,AHeight,ASchool,AFirstMovie,ALover) values ('나탈리 포트만',1981,160,'히 브리대','레옹','y');
 insert into ForeignActress (AName,ABirth,AHeight,ASchool,AFirstMovie,ALover) values ('키이라 나이틀리',1985,170,' 에셔대','로얄...','y');
-insert into ForeignActress (AName,ABirth,AHeight,ASchool,AFirstMovie,ALover) values ('엠마 왓슨',1990,165,'브라운 대 ,'해리포터와...','n');
+insert into ForeignActress (AName,ABirth,AHeight,ASchool,AFirstMovie,ALover) values ('엠마 왓슨',1990,165,'브라운 대' ,'해리포터와...','n');
 insert into ForeignActress (AName,ABirth,AHeight,ASchool,AFirstMovie,ALover) values ('크리스틴 스튜어트',1990,165 '고졸','써틴 이어','n');
 
 3.update
@@ -17,3 +17,5 @@ update ForeignActress set AFirstMovie='Royal Celebration' where AName='키이라
 
 4.delete
 delete from ForeignActress where AName='크리스틴 스튜어트';
+
+--마지막 수정 :2014092
