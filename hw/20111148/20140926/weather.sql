4c219a8596deaf5a4700f3f63974855f34f061e1
이예지 <20111148@prism.duksung.ac.kr>
2014-09-30 15:37:12 +0900

    9월30일 4.weather

---
+++ hw/20111148/20140926/weather.sql
@@ -0,0 +1,13 @@
+//2014년 9월 30일 네번째 테이블 weather
+//1.create
+create table weather (city varchar(255),wcondition varchar(255), tempAM int, tempPM int, precipi int);
+//2.insert
+insert into weather (city,wcondition,tempAM,tempPM,precipi) values ('서울 도봉구','구름조금',13,23,10);
+insert into weather (city,wcondition,tempAM,tempPM,precipi) values ('충남 아산','구름많음',15,24,20);
+insert into weather (city,wcondition,tempAM,tempPM,precipi) values ('독도','흐림',17,18,60);
+insert into weather (city,wcondition,tempAM,tempPM,precipi) values ('제주도 서귀포','흐림',22,25,60);
+insert into weather (city,wcondition,tempAM,tempPM,precipi) values ('경남 진주','구름많음',17,24,20);
+//3.update
+update weather set city='경기도 고양시' where tempAM=22;
+//4.delete
+delete from weather where city ='독도';
