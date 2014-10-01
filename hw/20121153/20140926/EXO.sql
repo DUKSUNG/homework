9e45b53ad5ffee4c9c25f60ac72a82a3bb0d4045
이한나 <20121153@prism.duksung.ac.kr>
2014-10-01 21:21:55 +0900

    첫번째 테이블_EXO.sql

---
+++ hw/20121153/20140926/EXO.sql
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+#*-* coding: UTF-8 -*-
+
+print "[create] 
+create table EXO (age int, name varchar(255), hometown varchar(255));"
+print "[insert]
+insert into EXO (age, name, hometown) values (23, '박찬열', '서울');
+insert into EXO (age, name, hometown) values (25, '시우민', '경기도');
+insert into EXO (age, name, hometown) values (22, '디오', '서울');
+insert into EXO (age, name, hometown) values (25, '루한', '북경');
+insert into EXO (age, name, hometown) values (21, '오세훈', '서울');"
+print "[update]
+update EXO set name='오세훈' where age=21;"
+print "[delete]
+delete from EXO where name='카이';"
