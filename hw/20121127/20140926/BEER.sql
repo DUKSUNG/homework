82246f9c3ca15b959522b30aab86c95d64a5eb2a
유송이 <20121127@prism.duksung.ac.kr>
2014-10-02 22:05:01 +0900

    2nd sql

---
+++ hw/20121127/20140926/BEER.sql
@@ -0,0 +1,16 @@
+use DS_20121127
+
+create table BEER( name varchar(255), degree int, rank int, favor varchar(255));
+
+insert into BEER (name, degree, rank, favor) values ('kloud', '5%', 1, 'smooth');
+insert into BEER (name, degree, rank, favor) values ('hite', 5, 2, 'strong');
+insert into BEER (name, degree, rank, favor) values ('cass light', 5, 3, 'strong');
+insert into BEER (name, degree, rank, favor) values ('OBlarger', 6, 4, 'very strong');
+insert into BEER (name, degree, rank, favor) values ('max', 5, 5, 'smooth');
+
+update BEER set favor='very smooth' where name='kloud';
+
+delete from BEER where rank=2;
+
+
+
