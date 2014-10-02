be269513f32341083ac4bc5a32f934137215ae81
김예슬 <20121164@prism.duksung.ac.kr>
2014-10-02 23:11:27 +0900

    첫번째

---
+++ hw/20121164/20140926/FRIEND.sql
@@ -0,0 +1,9 @@
+#*-* coding: UTF8 -*-
+create table FRIEND(id int, name varchar(255),height int);
+insert into FRIEND(id, name, height) values(1,'박예린',167);
+insert into FRIEND(id, name, height) values(2,'이채린',163);
+insert into FRIEND(id, name, height) values(3,'김효원',160);
+insert into FRIEND(id, name, height) values(4,'김수현',172);
+insert into FRIEND(id, name, height) values(5,'박보화',165);
+update FRIEND set name='장승언' where id=4;
+delete from FRIEND where name='박보화';
