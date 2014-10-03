f17565e71a4015b22631da725fbceca76f6332c5
김예슬 <20121164@prism.duksung.ac.kr>
2014-10-04 01:46:50 +0900

    두번째

---
+++ hw/20121164/20140926/PROGRAMSHOW.sql
@@ -0,0 +1,25 @@
+<create>
+create table PROGRAMSHOW(rank int, name varchar(255), channel varchar(255),day varchar(255),starttime int);
+
+<insert>
+insert into PROGRAMSHOW(rank,name,channel,day,starttime) values (1,'해피 선데이','KBS2','SUN',4);
+
+insert into PROGRAMSHOW(rank,name,channel,day,starttime) values (2,'일밤','MBC','SUN',4);
+
+insert into PROGRAMSHOW(rank,name,channel,day,starttime) values (3,'무한도전','MBC','SAT',6);
+
+insert into PROGRAMSHOW(rank,name,channel,day,starttime) values (4,'가요무대','KBS1','MON',10);
+
+insert into PROGRAMSHOW(rank,name,channel,day,starttime) values (5,'정글의법칙','SBS','FRI',10);
+
+insert into PROGRAMSHOW(rank,name,channel,day,starttime) values (6,'꽃보다청춘','TVN','FRI',9);
+
+insert into PROGRAMSHOW(rank,name,channel,day,starttime) values (7,'해피투게더3','KBS2','THU',11);
+
+insert into PROGRAMSHOW(rank,name,channel,day,starttime) values (8,'나혼자산다','MBC','FRI',11);
+
+<update>
+update PROGRAMSHOW set name='세바퀴' where rank=8;
+
+<delete>
+delete from PROGRAMSHOW where channel ='TVN';
