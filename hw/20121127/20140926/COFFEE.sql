0df30959b8c9fc2e9dbe82193b91f1cedd651f0a
유송이 <20121127@prism.duksung.ac.kr>
2014-10-04 09:56:37 +0900

    3rd sql

---
+++ hw/20121127/20140926/COFFEE.sql
@@ -0,0 +1,15 @@
+use DS_20121127;
+
+create table COFFEE (name varchar(255), syrup varchar(255), size varchar(255));
+
+insert COFFEE (name, syrup, size) values ('아메리카노', 'no syrup', 'small, tall, grande');
+insert COFFEE (name, syrup, size) values ('카페라떼', 'no syrup', 'tall, grande');
+insert COFFEE (name, syrup, size) values ('카라멜 마끼야또', 'caramel syrup', 'tall, grande');
+insert COFFEE (name, syrup, size) values ('카페모카', 'choco syrup', 'tall, grande');
+insert COFFEE (name, syrup, size) values ('샤케라토', 'no syrup', 'tall');
+
+update COFFEE set syrup='can add syrup' where syrup='no syrup';
+
+delete from COFFEE where name='샤케라토';
+
+
