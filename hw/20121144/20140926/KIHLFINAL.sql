9a9da6dbf85ccbb803adf2f45673aaf0b30030e5
윤혜정 <20121144@prism.duksung.ac.kr>
2014-10-04 12:06:33 +0900

    과제2

---
+++ hw/20121144/20140926/KIHLFINAL.sql
@@ -0,0 +1,14 @@
+use DS_20121144;
+
+create table KIHLFINAL(name varchar(255), team varchar(255), goal int, assist int, school varchar(255));
+
+insert into KIHLFINAL(name, team, goal, assist, school) values('함정우', 'WAVES', 2, 2, '경희대학교');
+insert into KIHLFINAL(name, team, goal, assist, school) values('Kang,Daniel', 'TITANS', 2, 2, '일본 토요대');
+insert into KIHLFINAL(name, team, goal, assist, school) values('황경필', 'INVICTUS', 1, 3, '광운대');
+insert into KIHLFINAL(name, team, goal, assist, school) values('Lee, Sung-Jun', 'TITANS', 2, 2, '하이원');
+insert into KIHLFINAL(name, team, goal, assist, school) values('Riley Nugent-Horgen', 'TITANS', 2, 1, 'NCAA III');
+
+update KIHLFINAL set team='WAVES' where name='함정우';
+
+delete from KIHLFINAL where team='TITANS';
+
