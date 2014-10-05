e072604aa7a1ed08baeccd56f96d06e8654c57f1
진실 <20121149@prism.duksung.ac.kr>
2014-10-05 19:07:27 +0900

    드라마

---
+++ hw/20121149/20140926/DRAMAS.sql
@@ -0,0 +1,19 @@
+create table DRAMAS(
+	dramas_rank int,
+	title varchar(255),
+	channel varchar(30),
+	ratings varchar(255)
+);
+
+insert into DRAMAS values(1,'왔다! 장보리','MBC', '33.8%');
+insert into DRAMAS values(2,'가족끼리 왜 이래','KBS2', '20.7%');
+insert into DRAMAS values(3,'akak','MBC', '18.0%');
+insert into DRAMAS values(4,'끝없는 사랑','SBS', '6.5%');
+insert into DRAMAS values(5,'기분 좋은 날','SBS', '4.9%');
+insert into DRAMAS values(6,'비밀의 문(재방송)','SBS', '2.7%');
+
+delete from DRAMAS where dramas_rank=6;
+
+update DRAMAS set title='마마' where dramas_rank=3;
+
+
