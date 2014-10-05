36f2c882ba88f8cf78a629b85d1a57b14cf81954
김명정 <20111588@prism.duksung.ac.kr>
2014-10-05 17:56:13 +0900

    세번째 테이블!

---
+++ hw/20111588/20140926/Animal.sql
@@ -0,0 +1,11 @@
+create table Animal (name varchar(255), legs int, sort varchar(255));
+
+insert into Animal (name, legs, sort) values ('토끼', 4, '토끼류');
+insert into Animal (name, legs, sort) values ('펭귄', 2, '조류');
+insert into Animal (name, legs, sort) values ('문어', 8, '연체동물');
+insert into Animal (name, legs, sort) values ('코브라', 0, '파충류');
+insert into Animal (name, legs, sort) values ('무당벌레', 60, '곤충류');
+
+update Animal set legs=6 where name='무당벌레';
+
+delete from Animal where sort='파충류';
