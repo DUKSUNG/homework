b60222e520138423ab468c89a0697a200ae570a9
윤희선 <20101217@prism.duksung.ac.kr>
2014-10-05 22:08:47 +0900

    첫번쨰 테이블 추가

---
+++ hw/20101217/20140926/EVENT.sql
@@ -0,0 +1,17 @@
+
+EVENT 테이블
+
+
+
+
+create table EVENT (id int , date int , event varchar(255));
+
+
+insert into EVENT(id,date,event) values(1,1,'설날');
+
+
+update EVENT set date=2 where id=2;
+
+
+delete from EVENT where name='없음';
+
