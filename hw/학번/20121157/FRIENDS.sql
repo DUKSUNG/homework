56112686cf60af7e2fe35409d34e8b8cc37f265a
김효은 <20121157@prism.duksung.ac.kr>
2014-10-05 22:56:54 +0900

    첫번째 과제이욥

---
+++ hw/학번/20121157/FRIENDS.sql
@@ -0,0 +1,18 @@
+[create]
+ create table FRIENDS (ID int, NAME varchar(255), GENDER varchar(1), AGE int, HEIGHT int);
+[insert]
+insert into FRIENDS (ID, NAME, GENDER, AGE, HEIGHT) values (1, '하지황', 'W', '23', '163');
+insert into FRIENDS (ID, NAME, GENDER, AGE, HEIGHT) values (2, '박유니', 'W', '23', '162');
+ insert into FRIENDS (ID, NAME, GENDER, AGE, HEIGHT) values (3, '고수경', 'W', '22', '154');
+insert into FRIENDS (ID, NAME, GENDER, AGE, HEIGHT) values (4, '강희원', 'W', '22', '160');
+ insert into FRIENDS (ID, NAME, GENDER, AGE, HEIGHT) values (5, '김효은', 'W', '23', '158');
+[update]
+update FRIENDS set name='김효니' where id=3;
+ update FRIENDS set name='고수경' where id=3;
+[delete]
+delete from FRIENDS where name='김효은';
+[insert]
+insert into FRIENDS (ID, NAME, GENDER, AGE, HEIGHT) values (5, '이지선', 'W', '23', '160');
+ insert into FRIENDS (ID, NAME, GENDER, AGE, HEIGHT) values (5, '이경진', 'W', '23', '167');
+update FRIENDS set id=6 where name='이경진';
+
