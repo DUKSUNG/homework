6462ac005f9ce9f4f348a6efe36c39ebf367dbdc
하지황 <20121134@prism.duksung.ac.kr>
2014-10-06 21:35:35 +0900

    첫번째 과제임다

---
+++ hw/학번/20121134/ACTOR.sql
@@ -0,0 +1,10 @@
+[create]create table STAR(id int, name varchar(255), age int(11), gender varchar(2),height int(11) );
+[insert]insert into ACTOR (id, name, age, gender, height) values(1, '연우진', 31, 남성, 185)
+insert into ACTOR (id, name, age, gender, height) values(2, '이진욱', '34', '남 성, '185');
+ insert into ACTOR (id, name, age, gender, height) values(3, '김태희', '35', '여성', '158');
+insert into ACTOR (id, name, age, gender, height) values(4, '한고은', '40', '여성', '174'); insert into ACTOR (id, name, age, gender, height) values(5, '정우성', '43', '남성', '180');
+[delete] delete from ACTOR where name='이진욱';
+[updete]update ACTOR set name='이정재' where id=5;
+
+
+
