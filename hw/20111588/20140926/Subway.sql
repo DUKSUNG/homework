36aab3f7651229cf3d285e68db9e1f6d40688c3c
김명정 <20111588@prism.duksung.ac.kr>
2014-10-07 21:29:02 +0900

    7일의 실습입니당!

---
+++ hw/20111588/20140926/Subway.sql
@@ -0,0 +1,14 @@
+create table Subway (line int, name varchar(255), university varchar(255));
+
+insert into Subway (line, name, university) values (4, '수유', '덕성여자대학교');
+insert into Subway (line, name, university) values (2, '왕십리', '한양대학교');
+insert into Subway (line, name, university) values (9, '흑석', '중앙대학교');
+insert into Subway (line, name, university) values (6, '고려대', '고려대학교');
+insert into Subway (line, name, university) values (1, '회기', '경히대학교');
+insert into Subway (line, name, universtiy) values (7, '건대입구', '건국대학교');
+
+update Subway set university='경희대학교' where line=1;
+
+delete from Subway where name='건대입구';
+
+
