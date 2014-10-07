4656900cb4eab09d935673f702892337027b03e6
진실 <20121149@prism.duksung.ac.kr>
2014-10-07 23:46:09 +0900

    실시간검색어

---
+++ hw/20121149/20140926/RankOfSearch.sql
@@ -0,0 +1,19 @@
+create table RankOfSearch(
+	rank int not null auto_increment,
+	context varchar(255),
+	pre_rank int,
+	primary key(rank)
+);
+
+insert into RankOfSearch values(1,'진재형',375);
+insert into RankOfSearch values(2,'10월 모의고사',207);
+insert into RankOfSearch values(3,'정유미',69);
+insert into RankOfSearch values(4,'10월 모의고사 등급컷',921);
+insert into RankOfSearch values(5,'진재형',66);
+insert into RankOfSearch values(6,'진재형',375);
+
+delete from RankOfSearch where rank=6;
+
+update RankOfSearch set context='북한' where rank=5;
+
+
