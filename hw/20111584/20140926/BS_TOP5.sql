79af9d07e61c9baa75e4818eed61b0500d6af37c
한수현 <20111584@prism.duksung.ac.kr>
2014-10-07 14:24:29 +0900

    third table

---
+++ hw/20111584/20140926/BS_TOP5.sql
@@ -0,0 +1,27 @@
+--@14.10.07 Best sellers at Kyobo book centre 
+
+
+--*create
+create table BS_TOP5 ( rank int, title varchar(255), Writer varchar(255), price varchar(10), star_rating varchar(10));
+
+--*insert
+
+
+insert into BS_TOP5 (rank, title, Writer, price, star_rating) values (1,'여자없는 남자들', '무라카미 하루키', '12,420', '★★★★☆');
+insert into BS_TOP5 (rank, title, Writer, price, star_rating) values (2,'나미야 잡화점의 기적', '히가시노 게이고', '14,800', '★★★★☆');
+insert into BS_TOP5 (rank, title, Writer, price, star_rating) values (3,'21세기 자본','토마 피케티','29,700','★★★★★');
+insert into BS_TOP5 (rank, title, Writer, price, star_rating) values (4,'비밀의 정원','조해너 배스포드','9,690','★★★★☆');
+insert into BS_TOP5 (rank, title, Writer, price, star_rating) values (5,'창문넘어 도망친 100세 노인','요나스 요나손','12,420','★★★☆☆');
+insert into BS_TOP5 (rank, title, Writer, price, star_rating) values (6,'메이즈 러너','제임스 대시너','10,540','★★★☆☆');
+
+
+--*update
+
+update BS_TOP5 set price='7,400' where rank=2;
+update BS_TOP5 set rank=5 where rank=6;
+
+--*delete
+
+delete from BS_TOP5 where rank=5;
+
+
