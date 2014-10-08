a8eebfde3ea9fa6e16785b35ec037a05540cef21
윤희선 <20101217@prism.duksung.ac.kr>
2014-10-08 22:08:46 +0900

    네번째 테이블 추가

---
+++ hw/20101217/20140926/BOOK.sql
@@ -0,0 +1,37 @@
+
+
+BOOK table 
+
+
+create table BOOK(rank int,name varchar(255),author varchar(255),publish varchar(255));
+
+
+
+
+insert into BOOK(rank,name,author,publish) values(1,'여자없는 남자들','무라카미 하루키','문학동네');
+
+
+insert into BOOK(rank,name,author,publish) values(2,'21세기 자본','토마 피게티','글항아리');
+
+
+insert into BOOK(rank,name,author,publish) values(3,'창문 넘어 도망친 100세 노인','요나스 요나손','열린책들');
+
+
+insert into BOOK(rank,name,author,publish) values(4,'원피스75권','Eiichiro Oda','대원씨아이');
+
+
+insert into BOOK(rank,name,author,publish) values(5,'비밀의정원','조셉','클');
+
+
+insert into BOOK(rank,name,author,publish) values(6,'어떤하루','신준모','프롬북스');
+
+
+
+update BOOK set name='조셉' where name='조나스 배스포드';
+
+
+delete from BOOK where name="원피스75권';
+
+
+
+
