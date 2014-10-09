90978a52f2e5d7c8ff7f0c1db81130d51433d110
김지연 <20121161@prism.duksung.ac.kr>
2014-10-09 20:57:17 +0900

    TVProgram에 관한 테이블 작성

---
+++ hw/20121161/20140926/TVProgram.sql
@@ -0,0 +1,16 @@
+#TVProgram에 관한 테이블을 작성하였음
+
+create table TVProgram (Program varchar(255), AirTime varchar(255), Channel varchar(255), Rating float);
+
+insert into TVProgram (Program, AirTime, Channel, Rating) values ('비정상회담', 'Mon, 23:00~', 'JTBC', 4.7);
+insert into TVProgram (Program, AirTime, Channel, Rating) values ('무한도전', 'Sat, 18:20~', 'MBC', 10.3);
+insert into TVProgram (Program, AirTime, Channel, Rating) values ('라디오스타', 'Wed, 23:15~', 'MBC', 5.5);
+insert into TVProgram (Program, AirTime, Channel, Rating) values ('해피투게더', 'THU, 11:10~', 'KBS2', 11.5);
+insert into TVProgram (Program, AirTime, Channel, Rating) values ('런닝맨', 'SUN, 18:10~', 'SBS', 10.1);
+
+update TVProgram set AirTime='Thu, 23:10' where Program='해피투게더';
+update TVProgram set AirTime='Sun, 18:10~' where Program='런닝맨';
+
+delete from TVProgram where Rating=5.5;
+
+
