ee411945c5e98adea5829ecdc1b27998c5c825d4
강민영 <20111154@prism.duksung.ac.kr>
2014-10-10 14:13:21 +0900

    4주차 과제 다섯번째-수정

--- hw/20111154/20140926/TimeTable.sql
+++ hw/20111154/20140926/TimeTable.sql
@@ -12,7 +12,7 @@ create table TimeTable (weekday varchar(128), time varchar(128), category varcha
 insert into TimeTable (weekday, time, category, subject, prof) values('Mon', '11:00-13:00', '전공', '인터넷해킹및보안', '강남희');
 insert into TimeTable (weekday, time, category, subject, prof) values('Mon', '16:00-18:00', '전공', '웹정보시스템', '박우창');
 insert into TimeTable (weekday, time, category, subject, prof) values('Tue', '09:00-12:00', '핵심교양', '인간과사상', '이재춘');
-insert into TimeTable (weekday, time, category, subject, prof) values('Wed', '13:00-15:00', '전공', '컴퓨터게임, '박태정');
+insert into TimeTable (weekday, time, category, subject, prof) values('Wed', '13:00-15:00', '전공', '컴퓨터게임', '박태정');
 insert into TimeTable (weekday, time, category, subject, prof) values('Fri', '14:00-17:00', '전공', '데이터베이스응용', '권혁진');
 insert into TimeTable (weekday, time, category, subject, prof) values('Wed', '12:00-13:00', '핵심교양', '문학속의여성들', '하명진');
 
