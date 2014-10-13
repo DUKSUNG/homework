c8ba2a024925bf3ee2847731d0ec01c6eedb112f
김지연 <20121161@prism.duksung.ac.kr>
2014-10-14 00:42:07 +0900

    20141010/첫번째 테이블작성

---
+++ hw/20121161/20141010/Lecture.sql
@@ -0,0 +1,13 @@
+create table Lecture (LectureID int AUTO_INCREMENT, Semester varchar(255), Name varchar(255), Grade float, primary key(LectureID));
+
+
+
+insert into Lecture (Semester, Name, Grade) values ('3학년 1학기', '3D 게임디자인', 4.5);
+insert into Lecture (Semester, Name, Grade) values ('3학년 1학기', '객체프로그래밍', 4.0);
+insert into Lecture (Semester, Name, Grade) values ('2학년 2학기', '비주얼프로그래밍', 3.5);
+insert into Lecture (Semester, Name, Grade) values ('1학년 2학기', '대학영어', 3.0);
+insert into Lecture (Semester, Name, Grade) values ('2학년 1학기', '네트워킹개론' 4.0);
+
+update Lecture set Grade=3.5 where LectureID=4;
+
+delete from Lecture where LectureID=3;
