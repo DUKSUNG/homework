f443c9cad5508db3d5097540acb509bf7f0c6394
한수현 <20111584@prism.duksung.ac.kr>
2014-10-14 14:26:34 +0900

    first table

---
+++ hw/20111584/20141010/Simpsons.sql
@@ -0,0 +1,21 @@
+
+create table Simpsons ( 
+	no INT AUTO_INCREMENT  not null,
+    name varchar(40) not null,
+	gender varchar(1) not null,
+	age int not null,
+	height int default 0,
+	primary key(no) 
+   );
+			
+
+
+insert into Simpsons (name, gender, age, height) values ( "Homer jay","M",39, "182cm");
+insert into Simpsons (name, gender, age, height) values ( "Marge Jacqueline","F", 39, "168");
+insert into Simpsons (name, gender, age, height) values ( "Bartholomew Jo-Jo","M", 10, "147");
+insert into Simpsons (name, gender, age, height) values ( "Lisa Marie","F", 8, "140");
+insert into Simpsons (name, gender, age, height) values ( "Margarret Maggie","F", 1, "50");
+
+update Simpsons set height = 150 where no = 3;
+
+delete from Simpsons where no = 5;
