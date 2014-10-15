52cb04e563dcb2d738e99be8fa48fdf5728a5ad5
김명정 <20111588@prism.duksung.ac.kr>
2014-10-15 10:14:10 +0900

    첫번째입니다

---
+++ hw/20111588/20141010/Class.sql
@@ -0,0 +1,11 @@
+create table Class (classroom int auto_increment, day varchar(255), subject varchar(255), students int, primary key(classroom));
+
+insert into Class values (339, "Friday", "데이터베이스", 33);
+insert into Class values (340, "Tuesday", "입체영상", 23);
+insert into Class values (336, "Wedneday", "서버프로그래밍", 33);
+insert into Class values (136, "Monday",  "인터넷해킹및보안", 18);
+insert into Class values (231, "Thursday", "과학사", 48);
+
+update Class set subject="데이터베이스응용" where classroom=339;
+
+delete from Class where classroom=231;
