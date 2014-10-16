59695ef59d86936abba1fec40b39042293477260
이한나 <20121153@prism.duksung.ac.kr>
2014-10-16 15:01:48 +0900

    세번째 primarykey 사용한 table

---
+++ hw/20121153/20141010/health.sql
@@ -0,0 +1,51 @@
+[CREATE] 
+ 
+ 
+ CREATE TABLE health( 
+	number INT NOT NULL AUTO_INCREMENT, 
+	sport VARCHAR(255), 
+	when50kg int, 
+	when60kg int, 
+	when70kg int, 
+	PRIMARY KEY (number) 
+	 ); 
+	 
+	 
+	 [INSERT] 
+	 
+	 
+	 INSERT INTO health (number, sport, when50kg, when60kg, when70kg) 
+	 VALUES(7, '산책', 66,78,90 ); 
+	 
+	 
+	 INSERT INTO health ( sport, when50kg, when60kg, when70kg) 
+	 VALUES('스트레칭 체조', 63,102,129 ); 
+
+	 INSERT INTO health ( sport, when50kg, when60kg, when70kg) 
+	 VALUES('자전거', 93,75,87 );
+
+	 INSERT INTO health ( sport, when50kg, when60kg, when70kg) 
+	 VALUES('춤추기', 102,123,144 );
+
+	 INSERT INTO health ( sport, when50kg, when60kg, when70kg) 
+	 VALUES('볼링', 75,99,105 );
+
+	INSERT INTO health ( sport, when50kg, when60kg, when70kg) 
+	 VALUES('요가', 63,75,87 );
+
+
+
+
+	 [UPDATE] 
+	 
+	 
+	 UPDATE health SET sport='자전거(보통속도로)'  WHERE number=9; 
+	 UPDATE health SET sport='산책(보통속도로 걷기)'  WHERE number=7;
+
+
+	 
+	 
+	 [DELETE] 
+	 
+	 
+	 DELETE from health where number=7; 
