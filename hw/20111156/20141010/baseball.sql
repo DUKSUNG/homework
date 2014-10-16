5af4951694dc8bcdddee6a5c3f520ddedb7cc4c9
김은지 <20111156@prism.duksung.ac.kr>
2014-10-16 09:35:09 +0900

    hw1

--- hw/20111156/20141010/baseball.sql
+++ hw/20111156/20141010/baseball.sql
@@ -7,12 +7,12 @@ create table baseball(
 				primary key(순위));
 
 
-insert baseball(순위,팀명,승,무,패) values (1,'삼성',77,3,45);
-insert baseball(순위,팀명,승,무,패) values (2,'넥센',75,2,48);
-insert baseball(순위,팀명,승,무,패) values (3,'NC',69,1,56);
-insert baseball(순위,팀명,승,무,패) values (4,'LG',62,2,62);
-insert baseball(순위,팀명,승,무,패) values (5,'SK',60,2,52);
-insert baseball(순위,팀명,승,무,패) values (6,'두산',57,1,67);
+insert baseball(팀명,승,무,패) values ('삼성',77,3,45);
+insert baseball(팀명,승,무,패) values ('넥센',75,2,48);
+insert baseball(팀명,승,무,패) values ('NC',69,1,56);
+insert baseball(팀명,승,무,패) values ('LG',62,2,62);
+insert baseball(팀명,승,무,패) values ('SK',60,2,52);
+insert baseball(팀명,승,무,패) values ('두산',57,1,67);
 
 update baseball set 패 =63 where 순위=1;
 
