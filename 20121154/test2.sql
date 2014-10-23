7d47096a605017216778e208bf75625758461a88
김유림 <20121154@prism.duksung.ac.kr>
2014-10-23 20:35:12 +0900

    테스트

---
+++ 20121154/test2.sql
@@ -0,0 +1,34 @@
+.
+├── 20140919
+│   ├── TEST
+│   ├── dwulip.html
+│   └── helloworld.py
+├── 20140922
+│   ├── TEST
+│   ├── TEST_SEL
+│   └── today01.py
+├── 20140926
+│   ├── 02Review
+│   └── 03Review
+├── notice.html
+├── test2.sql
+└── test3.sql
+
+3 directories, 11 files
+.
+├── 20140919
+│   ├── TEST
+│   ├── dwulip.html
+│   └── helloworld.py
+├── 20140922
+│   ├── TEST
+│   ├── TEST_SEL
+│   └── today01.py
+├── 20140926
+│   ├── 02Review
+│   └── 03Review
+├── notice.html
+├── test2.sql
+└── test3.sql
+
+3 directories, 11 files
