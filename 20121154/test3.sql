16ec2f6f084e61212e96288308e937ea679a82e7
김유림 <20121154@prism.duksung.ac.kr>
2014-10-23 20:40:37 +0900

    test

---
+++ 20121154/test3.sql
@@ -0,0 +1,9 @@
+합계 60
+drwxr-xr-x 5 20121154 students  4096 10월 23 20:24 .
+drwxr-xr-x 5 20121154 students  4096 10월 23 19:35 ..
+drwxr-xr-x 2 20121154 students  4096  9월 19 16:48 20140919
+drwxr-xr-x 2 20121154 students  4096  9월 22 17:38 20140922
+drwxr-xr-x 2 20121154 students  4096  9월 26 10:03 20140926
+-rwxr-xr-x 1 20121154 students 33080 10월 17 16:27 notice.html
+-rwxr-xr-x 1 20121154 students   428 10월 23 20:23 test2.sql
+-rw-r--r-- 1 20121154 students     0 10월 23 20:24 test3.sql
