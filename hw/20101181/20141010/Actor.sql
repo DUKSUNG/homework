6bd3bab7668c3dcd4b75b7367913a80c33121830
김대현 <20101181@prism.duksung.ac.kr>
2014-10-30 18:44:12 +0900

    다섯번째 테이블 수정

--- hw/20101181/20141010/Actor.sql
+++ hw/20101181/20141010/Actor.sql
@@ -1,13 +1,13 @@
-DS_20101181;
+drop table Actor;
 
 create table Actor(이름 varchar(255), 키 int, 생년월일 varchar(10), 최근작품 varchar(255));
 
-insert into(이름, 키, 생년월일, 최근작품) values('조인성', 186, '1981.07.28', '괜찮아 사랑이야');
-insert into(이름, 키, 생년월일, 최근작품) values('박서준', '185', '1988.12.16', '마녀의 연애');
-insert into(이름, 키, 생년월일, 최근작품) values('성준', 164, '1990.7.10', '연애의 발견');
-insert into(이름, 키, 생년월일, 최근작품) values('송준기', 178, '1985.09.19', '착한남자');
-insert into(이름, 키, 생년월일, 최근작품) values('유연석', 183, '1984.04.11', '꽃보다 청춘');
-insert into(이름, 키, 생년월일, 최근작품) values('이민기', 183, '1985.01.16', '몬스터');
+insert into Actor(이름, 키, 생년월일, 최근작품) values('조인성', 186, '1981.07.28', '괜찮아 사랑이야');
+insert into Actor(이름, 키, 생년월일, 최근작품) values('박서준', 185, '1988.12.16', '마녀의 연애');
+insert into Actor(이름, 키, 생년월일, 최근작품) values('성준', 164, '1990.7.10', '연애의 발견');
+insert into Actor(이름, 키, 생년월일, 최근작품) values('송준기', 178, '1985.09.19', '착한남자');
+insert into Actor(이름, 키, 생년월일, 최근작품) values('유연석', 183, '1984.04.11', '꽃보다 청춘');
+insert into Actor(이름, 키, 생년월일, 최근작품) values('이민기', 183, '1985.01.16', '몬스터');
 
 update Actor set 키='187' where 이름='성준';
 
