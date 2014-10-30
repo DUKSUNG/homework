3c20b5f75835d9cdbcccc82b30262c540abcb21f
박윤진 <20111129@prism.duksung.ac.kr>
2014-10-30 14:12:06 +0900

    20141010 에러 수정

--- hw/20111129/20141010/CAR.sql
+++ hw/20111129/20141010/CAR.sql
@@ -1,9 +1,11 @@
-//2014.10.16 첫번째 테이블 CAR
+drop table CAR;
 
-//1.create
+-- 2014.10.16 첫번째 테이블 CAR
+
+-- 1.create
 create table CAR (num int auto_increment,name varchar(255), company text,cc int, hp varchar(255),primary key(num));
 
-//2.insert
+-- 2.insert
 insert into CAR (name,company,cc,hp) values('2014 그랜저','현대자동차',2500,'190~270');
 insert into CAR (name,company,cc,hp) values('2015 아우디 A6','아우디',2000,'150~320');
 insert into CAR (name,company,cc,hp) values('2014 임팔라','쉐보레','3000','196~305');
@@ -12,8 +14,8 @@ insert into CAR (name,company,cc,hp) values('2015 카니발','기아자동차','
 insert into CAR (name,company,cc,hp) values('2014 미니 쿠퍼','BMW 미니',1500,'116~192');
 insert into CAR (name,company,cc,hp) values('2014 혼다 어코드','혼다','2000','188~282');
 
-//3.update
+-- 3.update
 update CAR set cc='3200' where num=7;
 
-//4.delete
+-- 4.delete
 delete from CAR where num=3;
--- hw/20111129/20141010/WORLDCUP_RANK.sql
+++ hw/20111129/20141010/WORLDCUP_RANK.sql
@@ -1,9 +1,12 @@
-//2014.10.17.두번째 테이블 WORLDCUP_RANK
+drop table WORLDCUP_RANK;
 
-//1.create
+
+-- 2014.10.17.두번째 테이블 WORLDCUP_RANK
+
+-- 1.create
 create table WORLDCUP_RANK (ranking int auto_increment,country varchar(255), fifa_ranking int, star_player text, primary key(ranking));
 
-//2.insert
+-- 2.insert
 insert into WORLDCUP_RANK (country, fifa_ranking, star_player) values('Brazil',11,'네이마르');
 insert into WORLDCUP_RANK (country, fifa_ranking, star_player) values('Spain',1,'사비 에르난데스');
 insert into WORLDCUP_RANK (country, fifa_ranking, star_player) values('Argentine',3,'리오넬 메시');
@@ -14,11 +17,11 @@ insert into WORLDCUP_RANK (country, fifa_ranking, star_player) values('Italy',8,
 insert into WORLDCUP_RANK (country, fifa_ranking, star_player) values('NETHERLANDS',8,'아르연 로번');
 insert into WORLDCUP_RANK (country, fifa_ranking, star_player) values('Belgium',5,'에당 아자르');
 
-//3.update
+-- 3.update
 update WORLDCUP_RANK set country='Uruguay' where ranking=6;
 update WORLDCUP_RANK set fifa_ranking=6 where ranking=6;
 
-//4.delete
+-- 4.delete
 delete from WORLDCUP_RANK where ranking=4;
 
 
