af855d350f593c0230b4746931800d1da0bd1971
김효은 <20121157@prism.duksung.ac.kr>
2014-10-31 09:48:18 +0900

    20141010 과제 수정했습니다!

--- hw/학번/20121157/20140926/WEBTOON.sql
+++ hw/학번/20121157/20140926/WEBTOON.sql
@@ -1,17 +1,19 @@
-[create]
- create table WEBTOON(TITLE varchar(255),WRITER varchar(255), MARK int, BOOKMARK int);
+drop table WEBTOON;
+
+-- [create]
  create table WEBTOON(TITLE varchar(255),WRITER varchar(255), MARK float, BOOKMARK int);
-[drop]
- drop table WEBTOON;
-[insert]
+
+-- [insert]
  INSERT INTO WEBTOON (TITLE, WRITER, MARK, BOOKMARK) VALUES ('치즈인더트랩', '순끼', '9.97', '99999');
  INSERT INTO WEBTOON (TITLE, WRITER, MARK, BOOKMARK) VALUES ('마음의 소리', '조석', '9.82', '99999');
  INSERT INTO WEBTOON (TITLE, WRITER, MARK, BOOKMARK) VALUES ('아랫집 시누이', '긴진', '9.95', '4466');
  INSERT INTO WEBTOON (TITLE, WRITER, MARK, BOOKMARK) VALUES ('오렌지마말레이드', '석우', '9.95', '11117');
  INSERT INTO WEBTOON (TITLE, WRITER, MARK, BOOKMARK) VALUES ('닥터 프로스트', '이종범', '9.94', '15747');
  INSERT INTO WEBTOON (TITLE, WRITER, MARK, BOOKMARK) VALUES ('오늘 밤은 어둠이 무서워요', '나이스 진', '9.97', '37151');
-[update]
+
+-- [update]
  update WEBTOON set WRITER='김진' where BOOKMARK='37151';
-[delete]
+
+-- [delete]
  delete from WEBTOON where TITLE='오렌지마말레이드';
 
