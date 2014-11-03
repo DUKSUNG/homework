36f90f25c9424737e0fc384997218cf553f734f1
이민진 <20111174@prism.duksung.ac.kr>
2014-11-03 23:56:49 +0900

    sql 정정과제

--- hw/20111174/20141010/news_2.sql
+++ hw/20111174/20141010/news_2.sql
@@ -1,9 +1,10 @@
+drop table news;
 create table news (
  num int auto_increment,
  topic    varchar(255) not null,
  title  varchar(255) not null,
  source varchar(255),
- primary key(num)     );
+ primary key(num));
 insert into news (topic, title, source) values ('정치', '남북 군사회담 전격 성사', '연합뉴스');
 insert into news (topic, title, source) values ('경제','경기지표 개선 vs 가계부채 상승', '문화일보');
 insert into news (topic, title, source) values ('사회', '이주영 에어포켓 전제 구조활동 사과드린다', '아이뉴스');
